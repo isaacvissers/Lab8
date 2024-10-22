188d29092de08547cebe05dd34b19c60dbb3301f
Isaac Vissers <ivissers@uaberta.ca>
2024-10-22 17:44:27 -0600

    added first test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,6 +1,7 @@
 package com.example.lab8;
 
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -11,6 +12,24 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
 
+    @Test
+    public void testAdd() {
+        CustomList list = MockCityList();
+        City myCity = new City("Edmonton", "Alberta");
+        assertEquals(list.getCount(), 0);
+        list.add(myCity);
+        assertEquals(list.getCount(), 1);
+//        assertTrue(list.hasCity(myCity));
+    }
 
 }
